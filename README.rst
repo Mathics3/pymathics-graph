9d289594310a8f962e67e8ca5a952f571909c2f2
rocky <rocky@gnu.org>
2020-11-05 01:13:47 -0500

    pymathics.graph: Mathic Graph functions

---
+++ README.rst
@@ -0,0 +1,11 @@
+Mathics Graph Module
+
+::
+
+   $ make develop  # or make install
+
+The above ``make`` command uses defaults the language to English. If
+you would like to install for another language set the variable
+``LANG``. For example:
+
+::
---
+++ pymathics/graph/__init__.py
@@ -0,0 +1,16 @@
+"""Pymathics Graph - Working with Graphs (Vertices and Edgies)
+
+This module provides functions and variables for workting with
+graphs.
+"""
+
+
+from pymathics.graph.__main__ import *
+from pymathics.graph.version import __version__
+
+pymathics_version_data = {
+    "author": "The Mathics Team",
+    "version": __version__,
+    "name": "Graph",
+    "requires": ["networkx"],
+}
