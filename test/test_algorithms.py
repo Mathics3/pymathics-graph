688d02e456a0f36b1fdc5d011f41d83ba9eade65
rocky <rb@dustyfeet.com>
2023-02-09 15:28:46 -0500

    Pull out pytests from algorithms doctest

--- test/test_algorithms.py
+++ test/test_algorithms.py
@@ -13,23 +13,44 @@ def setup_module(module):
 
 def test_connected_components():
     for str_expr, str_expected in [
-            ("PlanarGraphQ[Graph[{}]]", "False"),
-            ("g = Graph[{1 -> 2, 2 -> 3, 3 <-> 4}];", "Null"),
-            ("SortList[ConnectedComponents[g]]", "{{1}, {2}, {3, 4}}"),
-            ("g = Graph[{1 -> 2, 2 -> 3, 3 -> 1}];", "Null"),
-            ("SortList[ConnectedComponents[g]]", "{{1, 2, 3}}"),
+        ("PlanarGraphQ[Graph[{}]]", "False"),
+        ("g = Graph[{1 -> 2, 2 -> 3, 3 <-> 4}];", "Null"),
+        ("SortList[ConnectedComponents[g]]", "{{1}, {2}, {3, 4}}"),
+        ("g = Graph[{1 -> 2, 2 -> 3, 3 -> 1}];", "Null"),
+        ("SortList[ConnectedComponents[g]]", "{{1, 2, 3}}"),
     ]:
         check_evaluation(str_expr, str_expected)
 
 
 def test_graph_distance():
-    for str_expr, str_expected in [
-        ("GraphDistance[{1 <-> 2, 2 <-> 3, 3 <-> 4, 2 <-> 4, 4 -> 5}, 1, 5]", "3"),
-        ("GraphDistance[{1 <-> 2, 2 <-> 3, 3 <-> 4, 4 -> 2, 4 -> 5}, 1, 5]", "4"),
+    for str_expr, str_expected, mess in [
+        (
+            "GraphDistance[{1 <-> 2, 2 <-> 3, 3 <-> 4, 2 <-> 4, 4 -> 5}, 1, 5]",
+            "3",
+            None,
+        ),
+        ("GraphDistance[{1 <-> 2, 2 <-> 3, 3 <-> 4, 4 -> 2, 4 -> 5}, 1, 5]", "4", None),
         # ("GraphDistance[{1 <-> 2, 2 <-> 3, 4 -> 3, 4 -> 2, 4 -> 5}, 1, 5]", "Infinity"),
         (
             "Sort[GraphDistance[{1 <-> 2, 2 <-> 3, 3 <-> 4, 2 <-> 4, 4 -> 5}, 3]]",
             "{0, 1, 1, 2, 2}",
+            None,
+        ),
+        (
+            "GraphDistance[{}, 1, 1]",
+            "GraphDistance[{}, 1, 1]",
+            [
+                "The vertex at position 2 in GraphDistance[{}, 1, 1] does not belong to "
+                "the graph at position 1."
+            ],
+        ),
+        (
+            "GraphDistance[{1 -> 2}, 3, 4]",
+            "GraphDistance[{1 -> 2}, 3, 4]",
+            [
+                "The vertex at position 2 in GraphDistance[{1 -> 2}, 3, 4] does not belong "
+                "to the graph at position 1."
+            ],
         ),
     ]:
-        check_evaluation(str_expr, str_expected)
+        check_evaluation(str_expr, str_expected, expected_messages=mess)
