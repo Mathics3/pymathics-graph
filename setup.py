72643e91a24e46351d9069c8f8045cb295199759
rocky <rb@dustyfeet.com>
2024-08-06 12:30:35 -0400

    For now keep pymathics-graph over mathics3-graph

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ long_description = read("README.rst") + "\n"
 is_PyPy = platform.python_implementation() == "PyPy"
 
 setup(
-    name="Mathics3-graph",
+    name="pymathics3-graph",
     version=__version__,  # noqa
     packages=find_namespace_packages(include=["pymathics.*"]),
     install_requires=[
